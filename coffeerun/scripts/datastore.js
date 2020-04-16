340d7a47ce3019ace5184b5ca4677c791842b2ea
TJReynolds88 <tj.reynolds1988@gmail.com>
2020-04-16 16:27:14 -0500

    Add files via upload

---
+++ coffeerun/scripts/datastore.js
@@ -0,0 +1,13 @@
+
+
+(function(){
+    'use strict';
+    var App = window.App || {};
+
+    function DataStore() {
+        console.log('running the DataStore function');
+        this.data = {};
+    }
+    App.DataStore = DataStore;
+    window.App = App; 
+})();
